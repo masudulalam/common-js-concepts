99ec2a92fc26ffe6c021b4ef0dcfbdad4ec06ca4
masudulalam <masuudulalam@gmail.com>
2024-04-23 04:03:17 +0600

    double and triple equal (== and ===) difference example

---
+++ triple-equal.js
@@ -0,0 +1,11 @@
+const first = [1, 2, 3];
+// const second = 1;
+const second = first;
+
+
+if(first === second) {
+    console.log('Values are equal');
+}
+else {
+    console.log('Values are not equal');
+}
