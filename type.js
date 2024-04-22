983a2e19f3833deec21a4b3e57afad8b78eb4588
masudulalam <masuudulalam@gmail.com>
2024-04-22 13:01:10 +0600

    added some data type of js

---
+++ type.js
@@ -0,0 +1,7 @@
+// dynamic type language
+const a = 1;;
+const b = 'All praise to Allah'; 
+const c = true;
+const ages = [5, 4, 6, 7, 8];
+const student = {id: 11, class: 2};
+console.log(typeof a, typeof b, typeof c, typeof ages, typeof student); 
