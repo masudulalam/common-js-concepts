4fa63b7bdf8c2d03f9d8f20041aabff3ac08f5df
masudulalam <masuudulalam@gmail.com>
2024-04-23 07:26:06 +0600

    added scop.js

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+print5();
+// print10();
+for(let i = 0; i < 5; i++){
+    // console.log(i);
+}
+// console.log(i);
+
+function print5 () {
+    console.log(5);
+}
+
+const print10 = function () {
+    console.log(10);
+}
