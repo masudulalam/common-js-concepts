9bb06f9a1f7141dbaf54a20ca5f0a5914c9a35b8
masudulalam <masuudulalam@gmail.com>
2024-04-30 09:27:54 +0600

    an update

--- arguments.js
+++ arguments.js
@@ -1,9 +1,9 @@
 function sum(a, b, c){
-    // console.log(arguments);
+    console.log(arguments);
     const result = a + b + c;
     return result;
 }
 
-console.log(arguments);
+// console.log(arguments);
 const total = sum(45, 89, 37, 25, 76, 90);
 console.log(total); 
