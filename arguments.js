7ef797fcfeca9c45505dcf5f894e2156aad804d3
masudulalam <masuudulalam@gmail.com>
2024-04-30 09:19:59 +0600

    added arguments example

--- arguments.js
+++ arguments.js
@@ -1,7 +1,9 @@
 function sum(a, b, c){
+    // console.log(arguments);
     const result = a + b + c;
     return result;
 }
 
-const total = sum(45, 89, 37);
+console.log(arguments);
+const total = sum(45, 89, 37, 25, 76, 90);
 console.log(total); 
